16c848bb698f11da21b446a811d4b3932b227d53
OUSSEMA GUERAMI <oussema.guerami@insat.ucar.tn>
2025-03-28 15:10:08 +0100

    fixing details component , make projetcs dynamic

--- front/src/app/Components/project-details/project-details.component.ts
+++ front/src/app/Components/project-details/project-details.component.ts
@@ -1,5 +1,7 @@
 import {Component, HostListener, Input} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {project} from "../../Models/project";
+import {ProjectsService} from "../../Service/projects.service";
 
 @Component({
   selector: 'app-project-details',
@@ -8,14 +10,19 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class ProjectDetailsComponent {
   projectId :number |undefined;
+  projects :project[] = [];
   selectedImage='/assets/img/details.png'
   carouselMainElement='/assets/img/details1.png'
   carouselOtherElements='/assets/img/details2.png'
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,private projectsService:ProjectsService) {
   }
   ngOnInit() {
     window.scrollTo(0, 0);
     this.projectId = +this.route.snapshot.paramMap.get('id')!;
+    this.projectsService.getProjects().subscribe(data =>{
+      this.projects = data;
+    });
+    console.log("hello",this.projects)
   }
 
 
