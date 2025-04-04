5dc3f79062be1def09cec60d86652a8a9cfdb4f5
OUSSEMA GUERAMI <oussema.guerami@insat.ucar.tn>
2025-04-04 20:51:53 +0100

    adding back end project

---
+++ back/Ecopilot/src/main/java/com/ppp/Ecopilot/EcopilotApplication.java
@@ -0,0 +1,13 @@
+package com.ppp.Ecopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcopilotApplication.class, args);
+	}
+
+}
