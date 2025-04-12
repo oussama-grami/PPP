d8ad5b65eaaad927f988f121cb37136c1be85c2d
Samar Ben Houidi <147640400+samar14bh@users.noreply.github.com>
2025-04-13 00:49:44 +0100

    backend code fixing

--- back/Ecopilot/src/main/java/com/ppp/Ecopilot/Controllers/EsgController.java
+++ back/Ecopilot/src/main/java/com/ppp/Ecopilot/Controllers/EsgController.java
@@ -12,8 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/esg")
