d863e5f1514f564dbff5aa4f24de1e719f788958
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2025-04-29 23:22:08 +0100

    some fixes
    in the near commits i will be enhancing the ui of the header and then making the ui interactive with the auth state and also making an auth service for the teammates to use when needed

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8087',
   websiteUrl: 'http://localhost:4200/',
-  keycloakurl:'http://localhost:8080'
+  keycloakurl:'http://localhost:8080/'
 };
