4760320d33c44da9aa39b8460cfcccbeaa260f91
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2025-06-08 17:29:48 +0100

    Update app.module.ts

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,100 +1,70 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './Components/home/home.component';
-import {EnergieComponent} from './Components/energie/energie.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CarburantComponent} from './Components/carburant/carburant.component';
-import {AeriensComponent} from './Components/aeriens/aeriens.component';
-import {FretComponent} from './Components/fret/fret.component';
-import {ConsommablesComponent} from './Components/consommables/consommables.component';
-import {
-  ImmobilisationsComponent
-} from './Components/immobilisations/immobilisations.component';
-import {PorteurComponent} from './Components/porteur/porteur.component';
-import {PanneauxComponent} from './Components/panneaux/panneaux.component';
-import {
-  ResultatCarboneComponent
-} from './Components/resultat-carbone/resultat-carbone.component';
-import {EsgComponent} from './Components/esg/esg.component';
-import {CarboneComponent} from './Components/carbone/carbone.component';
-import {ResultESGComponent} from './Components/result-esg/result-esg.component';
-import {NavAcceuilComponent} from './Components/nav-acceuil/nav-acceuil.component';
-import {ESGdiagramComponent} from './Components/esgdiagram/esgdiagram.component';
-import {ArboricultureComponent} from './Components/arboriculture/arboriculture.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {MarketplaceComponent} from './Components/marketplace/marketplace.component';
-import {FilterItemComponent} from './Components/filter-item/filter-item.component';
-import {ProjectAdComponent} from './Components/project-ad/project-ad.component';
-import {CommonModule} from '@angular/common';
-import {
-  ProjectDetailsComponent
-} from './Components/project-details/project-details.component';
-import {CheckoutComponent} from './Components/checkout/checkout.component';
-import {FooterComponent} from './Components/footer/footer.component';
-import {PaymentComponent} from './Components/payment/payment.component';
-import {ProjectOwnerComponent} from './Components/project-owner/project-owner.component';
-import {ResourcesComponent} from './Components/resources/resources.component';
-import {InfoComponent} from './Components/info/info.component';
-import {
-  MarketplaceBlogComponent
-} from './Components/marketplace-blog/marketplace-blog.component';
-import {
-  CarbonOffsetBlogComponent
-} from './Components/carbon-offset-blog/carbon-offset-blog.component';
-import {
-  CarbonCoBenifitsBlogComponent
-} from './Components/carbon-co-benifits-blog/carbon-co-benifits-blog.component';
-import {RoadmapComponent} from './Components/roadmap/roadmap.component';
-import {EsgHeaderComponent} from './Components/esg-header/esg-header.component';
-import {
-  EsgAssessmentComponent
-} from './Components/esg-assessment/esg-assessment.component';
-import {PaginationComponent} from './Components/pagination/pagination.component';
-import {PriceDetailsComponent} from './Components/price-details/price-details.component';
-import {EsgOverviewComponent} from './Components/esg-overview/esg-overview.component';
-import {
-  ProjectsCarouselComponent
-} from "./Components/projects-carousel/projects-carousel.component";
-import {InfoSectionComponent} from './Components/InfoSection/InfoSection.component';
-import {
-  CarbonPredictionInfoComponent
-} from './Components/CarbonPredictionInfo/CarbonPredictionInfo.component';
-import {
-  HistoricalCarbonForecastComponent
-} from './Components/HistoricalCarbonForecast/HistoricalCarbonForecast.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
-import {TransactionsComponent} from './Components/transactions/transactions.component';
-import {
-  InstructionsListComponent
-} from "./Components/instructions-list/instructions-list.component";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {EventFormComponent} from './Components/event-form/event-form.component';
-import {
-  EventFootprintComponent
-} from './Components/event-footprint/event-footprint.component';
-import {EventResultComponent} from './Components/event-result/event-result.component';
-import {
-  PaymentConfirmationComponent
-} from './Components/payment-confirmation/payment-confirmation.component';
-import {
-  EcoLoadingSpinnerComponent
-} from './Components/eco-loading-spinner/eco-loading-spinner.component';
-import {FormatDescriptionPipe} from './pipes/format-description.pipe';
-import {EventHistoryComponent} from './Components/event-history/event-history.component';
-import {
-  CarbonHistoryComponent
-} from './Components/carbon-history/carbon-history.component';
-import {EsgHistoryComponent} from './Components/esg-history/esg-history.component';
-import {ApiModule} from './api/api.module';
-import {environment} from 'src/environments/environment';
-import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
-import {HttpTokenInterceptor} from './keycloaktoken.interceptor';
-import {SpinnerComponent} from "./Components/spinner/spinner.component";
-import {CompanyOwnerFormComponent} from "./Components/company-owner-form/company-owner-form.component";
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { EnergieComponent } from './Components/energie/energie.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CarburantComponent } from './Components/carburant/carburant.component';
+import { AeriensComponent } from './Components/aeriens/aeriens.component';
+import { FretComponent } from './Components/fret/fret.component';
+import { ConsommablesComponent } from './Components/consommables/consommables.component';
+import { ImmobilisationsComponent } from './Components/immobilisations/immobilisations.component';
+import { PorteurComponent } from './Components/porteur/porteur.component';
+import { PanneauxComponent } from './Components/panneaux/panneaux.component';
+import { ResultatCarboneComponent } from './Components/resultat-carbone/resultat-carbone.component';
+import { EsgComponent } from './Components/esg/esg.component';
+import { CarboneComponent } from './Components/carbone/carbone.component';
+import { ResultESGComponent } from './Components/result-esg/result-esg.component';
+import { NavAcceuilComponent } from './Components/nav-acceuil/nav-acceuil.component';
+import { ESGdiagramComponent } from './Components/esgdiagram/esgdiagram.component';
+import { ArboricultureComponent } from './Components/arboriculture/arboriculture.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MarketplaceComponent } from './Components/marketplace/marketplace.component';
+import { FilterItemComponent } from './Components/filter-item/filter-item.component';
+import { ProjectAdComponent } from './Components/project-ad/project-ad.component';
+import { CommonModule } from '@angular/common';
+import { ProjectDetailsComponent } from './Components/project-details/project-details.component';
+import { CheckoutComponent } from './Components/checkout/checkout.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { PaymentComponent } from './Components/payment/payment.component';
+import { ProjectOwnerComponent } from './Components/project-owner/project-owner.component';
+import { ResourcesComponent } from './Components/resources/resources.component';
+import { InfoComponent } from './Components/info/info.component';
+import { MarketplaceBlogComponent } from './Components/marketplace-blog/marketplace-blog.component';
+import { CarbonOffsetBlogComponent } from './Components/carbon-offset-blog/carbon-offset-blog.component';
+import { CarbonCoBenifitsBlogComponent } from './Components/carbon-co-benifits-blog/carbon-co-benifits-blog.component';
+import { RoadmapComponent } from './Components/roadmap/roadmap.component';
+import { EsgHeaderComponent } from './Components/esg-header/esg-header.component';
+import { EsgAssessmentComponent } from './Components/esg-assessment/esg-assessment.component';
+import { PaginationComponent } from './Components/pagination/pagination.component';
+import { PriceDetailsComponent } from './Components/price-details/price-details.component';
+import { EsgOverviewComponent } from './Components/esg-overview/esg-overview.component';
+import { ProjectsCarouselComponent } from './Components/projects-carousel/projects-carousel.component';
+import { InfoSectionComponent } from './Components/InfoSection/InfoSection.component';
+import { CarbonPredictionInfoComponent } from './Components/CarbonPredictionInfo/CarbonPredictionInfo.component';
+import { HistoricalCarbonForecastComponent } from './Components/HistoricalCarbonForecast/HistoricalCarbonForecast.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { TransactionsComponent } from './Components/transactions/transactions.component';
+import { InstructionsListComponent } from './Components/instructions-list/instructions-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventFormComponent } from './Components/event-form/event-form.component';
+import { EventFootprintComponent } from './Components/event-footprint/event-footprint.component';
+import { EventResultComponent } from './Components/event-result/event-result.component';
+import { PaymentConfirmationComponent } from './Components/payment-confirmation/payment-confirmation.component';
+import { EcoLoadingSpinnerComponent } from './Components/eco-loading-spinner/eco-loading-spinner.component';
+import { FormatDescriptionPipe } from './pipes/format-description.pipe';
+import { EventHistoryComponent } from './Components/event-history/event-history.component';
+import { CarbonHistoryComponent } from './Components/carbon-history/carbon-history.component';
+import { EsgHistoryComponent } from './Components/esg-history/esg-history.component';
+import { ApiModule } from './api/api.module';
+import { environment } from 'src/environments/environment';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { HttpTokenInterceptor } from './keycloaktoken.interceptor';
+import { SpinnerComponent } from './Components/spinner/spinner.component';
+import { CompanyOwnerFormComponent } from './Components/company-owner-form/company-owner-form.component';
 import { RegisterCompanyOwnerComponent } from './Components/register-company-owner/register-company-owner.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { RedirectGuard } from './guards/redirect.guard';
